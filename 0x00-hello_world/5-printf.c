963348ef021aef322914358ba04b9da50fcefc6a
mydemilade <ayunoluwademilade@gmail.com>
2023-04-20 19:50:09 -0700

    0-preprocessor

--- 0x00-hello_world/5-printf.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- * main - this is a program that prints a line using the printf function
- * Return : 0
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
