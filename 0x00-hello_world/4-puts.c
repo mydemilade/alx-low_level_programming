7ac38b610387fdd58cc6a46441f6f7013af3f998
mydemilade <ayunoluwademilade@gmail.com>
2023-04-20 20:28:23 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program prints a line that provide function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
