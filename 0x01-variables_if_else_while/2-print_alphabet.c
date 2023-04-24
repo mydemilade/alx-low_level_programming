12a8376475cb2e044df6b75bb46b974a4d5af697
mydemilade <ayunoluwademilade@gmail.com>
2023-04-24 10:41:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - A progrom that prints alphabet in lowercase
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
