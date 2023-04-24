1d9e8eea1df21500ef79b1a1b44716d5f76b7d90
mydemilade <ayunoluwademilade@gmail.com>
2023-04-24 15:09:50 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
