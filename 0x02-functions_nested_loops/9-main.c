87ff238f3b34703f207f6b82ad2bea0b85014bc1
mydemilade <ayunoluwademilade@gmail.com>
2023-04-25 14:22:07 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    times_table();
+    return (0);
+}
+julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 9-main.c 9-times_table.c -o 9-times_table
+ulien@ubuntu:~/0x02$ ./9-times_table | cat -e
+0,  0,  0,  0,  0,  0,  0,  0,  0,  0$
+0,  1,  2,  3,  4,  5,  6,  7,  8,  9$
+0,  2,  4,  6,  8, 10, 12, 14, 16, 18$
+0,  3,  6,  9, 12, 15, 18, 21, 24, 27$
+0,  4,  8, 12, 16, 20, 24, 28, 32, 36$
+0,  5, 10, 15, 20, 25, 30, 35, 40, 45$
+0,  6, 12, 18, 24, 30, 36, 42, 48, 54$
+0,  7, 14, 21, 28, 35, 42, 49, 56, 63$
+0,  8, 16, 24, 32, 40, 48, 56, 64, 72$
+0,  9, 18, 27, 36, 45, 54, 63, 72, 81$
+julien@ubuntu:~/0x02$ ./9-times_table | tr ' ' . | cat -e
+0,..0,..0,..0,..0,..0,..0,..0,..0,..0$
+0,..1,..2,..3,..4,..5,..6,..7,..8,..9$
+0,..2,..4,..6,..8,.10,.12,.14,.16,.18$
+0,..3,..6,..9,.12,.15,.18,.21,.24,.27$
+0,..4,..8,.12,.16,.20,.24,.28,.32,.36$
+0,..5,.10,.15,.20,.25,.30,.35,.40,.45$
+0,..6,.12,.18,.24,.30,.36,.42,.48,.54$
+0,..7,.14,.21,.28,.35,.42,.49,.56,.63$
+0,..8,.16,.24,.32,.40,.48,.56,.64,.72$
+0,..9,.18,.27,.36,.45,.54,.63,.72,.81$
+julien@ubuntu:~/0x02$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x02-functions_nested_loops
+File: 9-times_table.c
+   
+10. a + b
+mandatory
+Write a function that adds two integers and returns the result.
+
+Prototype: int add(int, int);
+julien@ubuntu:~/$ cat 10-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
