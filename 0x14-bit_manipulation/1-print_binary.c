032f1c3cad95f265fe79c2b14dc618abd27acd3f
mydemilade <ayunoluwademilade@gmail.com>
2023-06-08 04:21:51 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,12 +7,12 @@
  */
 void print_binary(unsigned long int n)
 {
-	int j, counter = 0;
+	int i, counter = 0;
 	unsigned long int current;
 
-	for (j = 63; j >= 0; j--)
+	for (i = 63; i >= 0; i--)
 	{
-		current = n >> j;
+		current = n >> i;
 
 		if (current & 1)
 		{
